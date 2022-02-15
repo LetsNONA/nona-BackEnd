ccb4d76dbf8700096d714e3d71fdb87097123dd9
choikangheon <choikangheon@naver.com>
2022-02-15 17:08:41 +0900

    init BackEnd(Spring)

---
+++ src/main/java/letsnona/nonabackend/NonaBackEndApplication.java
@@ -0,0 +1,13 @@
+package letsnona.nonabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/letsnona/nonabackend/NonaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package letsnona.nonabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
