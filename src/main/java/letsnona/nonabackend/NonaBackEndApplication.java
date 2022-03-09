24d88464f3b7eb0f79f7e4c95a3d1332248f6993
Choi Kang Heon <kangheon3@gmail.com>
2022-03-09 17:53:12 +0900

    mysql connect jpa

--- src/main/java/letsnona/nonabackend/NonaBackEndApplication.java
+++ src/main/java/letsnona/nonabackend/NonaBackEndApplication.java
@@ -2,12 +2,18 @@ package letsnona.nonabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NonaBackEndApplication {
+private static final String mainYml = "local";
 
     public static void main(String[] args) {
-        SpringApplication.run(NonaBackEndApplication.class, args);
+        new SpringApplicationBuilder(NonaBackEndApplication.class)
+                .properties(
+                        "spring.config.location=" + "classpath:/application-"+mainYml+".yml"
+
+                ).run(args);
     }
 
 }
