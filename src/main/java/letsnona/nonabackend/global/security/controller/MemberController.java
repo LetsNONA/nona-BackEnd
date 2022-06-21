91ff5b472cca63153d30c139cf75ec7ae0706a13
choikangheon <kangheon3@gmail.com>
2022-06-21 22:42:13 +0900

    get Point

--- src/main/java/letsnona/nonabackend/global/security/controller/MemberController.java
+++ src/main/java/letsnona/nonabackend/global/security/controller/MemberController.java
@@ -23,30 +23,8 @@ public class MemberController {
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	// 모든 사람이 접근 가능
-	@GetMapping("/home")
-	public String home() {
-		return "hello";
-	}
-	
-	// Tip : JWT를 사용하면 UserDetailsService를 호출하지 않기 때문에 @AuthenticationPrincipal 사용 불가능.
-	// 왜냐하면 @AuthenticationPrincipal은 UserDetailsService에서 리턴될 때 만들어지기 때문이다.
-	
-	// 유저 혹은 매니저 혹은 어드민이 접근 가능
-	@GetMapping("user")
-	public String user(Authentication authentication) {
-		PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
-		System.out.println("principal : "+principal.getUser().getId());
-		System.out.println("principal : "+principal.getUser().getUsername());
-		System.out.println("principal : "+principal.getUser().getPassword());
-		System.out.println("principal : "+principal.getUser().getRoles());
-		return "user";
-	}
-	
-	// 매니저 혹은 어드민이 접근 가능
-	@GetMapping("manager/reports")
-	public String reports() {
-		return "reports";
-	}
+
+
 	
 	// 어드민이 접근 가능
 	@GetMapping("admin/users")
